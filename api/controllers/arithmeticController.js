ebb575647f9d6e9bffa683c89fe7b4402b832fb9
Cathy-Dew <63077331+Cathy-Dew@users.noreply.github.com>
2023-03-14 20:04:42 -0700

    Update arithmeticController.js
    modify the add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -18,6 +18,7 @@ exports.calculate = function(req, res) {
   };
 
   // Determine the operation
+  //add more comments
 
   if (! req.query.operation) {
     throw new Error("Unspecified operation");
